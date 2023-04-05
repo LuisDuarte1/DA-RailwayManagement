eb3186cf691570a2fd6d566722687f98b2e9a654
Luís Duarte <luis.duarte.2003.13@hotmail.com>
2023-04-05 22:14:33 +0100

    Add menus and helper functions to get user input

--- src/Menu.h
+++ src/Menu.h
@@ -3,6 +3,8 @@
 
 
 #include <vector>
+#include "graph/Graph.h"
+#include "LineFailures.h"
 
 class Menu {
 public:
@@ -10,10 +12,17 @@ public:
     void mainMenu();
     void clearScreen();
 private:
+    Graph * graph;
 
     void printOptions(std::vector<std::string> options);
 
     bool validOption(char option, int max);
+    void maximumTrainsReducedConnectivityMenu();
+    void reportFailureSegmentMenu();
+    Vertex * getValidStation(std::string displayQuery);
+    Edge * getValidSegment(std::string displayQuery);
+    bool getBooleanInputFromUser(std::string displayString, bool defaultEnter);
+    int getIntegerInputFromUser(std::string displayString);
 };
 
 
