b597a7d2f82921f4f504af7351597a94ac009a89
Luís Jesus <luisjesus__2003@outlook.com>
2023-04-08 20:39:59 +0100

    moved documentation to header files

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,13 +1,6 @@
 #include <vector>
 #include <algorithm>
 
-/**
- * Computes the weights for each value in the vector using a linear scaling
- * Time Complexity: O(n) being n the number of values
- * @param values Vector of values
- * @param weights Vector of weights
- * @return The sum of the weights to be used to compute the average
- */
 float computeWeights(const std::vector<int>& values, std::vector<float> &weights) {
     int maxVal = *std::max_element(values.begin(), values.end());
     int minVal = *std::min_element(values.begin(), values.end());
